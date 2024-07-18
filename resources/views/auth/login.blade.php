718191d3bb665e4cfe895ec854cfb0dd965a609a
ByteNinja <pijayblog@gmail.com>
2024-07-18 14:56:29 +0700

    Create login.blade.php

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <br>
+        <label>Password:</label>
+        <input type="password" name="password" required>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
