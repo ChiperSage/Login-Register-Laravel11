eae796c5287c1c1d681f4f0b8b8a90037155ad8f
ByteNinja <pijayblog@gmail.com>
2024-07-18 14:47:11 +0700

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+use App\Http\Controllers\AuthController;
+
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [AuthController::class, 'register']);
