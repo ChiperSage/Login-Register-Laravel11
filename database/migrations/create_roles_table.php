211b39fac19a5a2c0ba0e0fb41aec85862df2be8
ByteNinja <pijayblog@gmail.com>
2024-07-18 15:34:12 +0700

    Create create_roles_table.php

---
+++ database/migrations/create_roles_table.php
@@ -0,0 +1,20 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id('role_id');
+            $table->string('role_name', 100)->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
