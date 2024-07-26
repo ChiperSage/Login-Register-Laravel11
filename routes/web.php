17a7559d9f7440645cf94593a932cf59bac9bc4e
ByteNinja <pijayblog@gmail.com>
2024-07-26 10:37:29 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
@@ -6,3 +7,7 @@ Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth')->group(function () {
+    Route::resource('user', UserController::class);
+});
