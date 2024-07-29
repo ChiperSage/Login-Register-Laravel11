ab868cf5f3408d3bed056c1f644a199a211025bf
ByteNinja <pijayblog@gmail.com>
2024-07-29 15:07:30 +0700

    Update User.php

--- app/models/User.php
+++ app/models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -10,23 +11,38 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $primaryKey = 'user_id';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    
+    protected $primaryKey = 'user_id'; // Menggunakan 'user_id' sebagai primary key
 
     protected $fillable = [
-        'username', 'password', 'login_attempts', 'last_login_attempt', 'remember_me_token', 'role_id', 'group_id'
+        'name', 'email', 'username', 'password', 'login_attempts', 'last_login_attempt', 'remember_me_token', 'role_id', 'group_id'
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
-        'password', 'remember_me_token',
+        'password',
+        'remember_token',
     ];
 
-    public function role()
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
     {
-        return $this->belongsTo(Role::class, 'role_id', 'role_id');
-    }
-
-    public function group()
-    {
-        return $this->belongsTo(Group::class, 'group_id', 'group_id');
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
