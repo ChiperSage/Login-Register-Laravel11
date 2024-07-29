fbecbb898f36311659e2f1f4d8516bfde56b3300
ByteNinja <pijayblog@gmail.com>
2024-07-29 15:08:23 +0700

    Update create_users_table.php

--- database/migrations/create_users_table.php
+++ database/migrations/create_users_table.php
@@ -1,30 +1,51 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('username', 100)->unique();
             $table->string('password');
             $table->integer('login_attempts')->default(0);
             $table->integer('last_login_attempt')->default(0);
             $table->string('remember_me_token', 255)->nullable();
             $table->timestamps();
+        });
 
-            $table->unsignedBigInteger('role_id')->nullable();
-            $table->unsignedBigInteger('group_id')->nullable();
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
 
-            $table->foreign('role_id')->references('role_id')->on('roles')->onDelete('set null');
-            $table->foreign('group_id')->references('group_id')->on('groups')->onDelete('set null');
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
     }
-}
+};
