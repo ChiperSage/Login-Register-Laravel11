8c9d642a7046d820abe469a53156982d17cb56d7
ByteNinja <pijayblog@gmail.com>
2024-07-30 16:07:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
@@ -9,5 +17,9 @@ Route::get('register', [AuthController::class, 'showRegistrationForm'])->name('r
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::resource('user', UserController::class);
+
+    Route::get('user/{user_id}/assign-role', [UserController::class, 'assignRole'])->name('user.assignRole');
+    Route::post('user/{user_id}/assign-role', [UserController::class, 'storeRoleAssignment'])->name('user.storeRoleAssignment');
 });
